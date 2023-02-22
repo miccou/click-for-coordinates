9e10b61a3681a87f95cf46f4bd94cff99e17e3fb
miccou <30512204+miccou@users.noreply.github.com>
2023-02-22 04:14:36 +0000

    initial setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'click-for-coordinates';
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/**/*.ts", "./src/app/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
