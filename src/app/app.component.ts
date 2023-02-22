c8af276dbad40ac7a61ee03cef6859814ccb9e40
miccou <30512204+miccou@users.noreply.github.com>
2023-02-22 04:19:30 +0000

    setup map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'click-for-coordinates';
+
+  private map: L.Map = L.map('map');
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3
+    });
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
