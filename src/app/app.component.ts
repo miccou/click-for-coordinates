2eb3de497214a72f7addee2bf1f850068562ef73
miccou <30512204+miccou@users.noreply.github.com>
2023-02-22 10:42:58 +0000

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,29 +4,37 @@ import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent implements AfterViewInit {
   private map!: L.Map;
 
   coordinates: L.LatLng[] = [];
+  line!: L.Polyline;
 
   coordinateOutputTypeForm: FormGroup;
   constructor(fb: FormBuilder) {
     this.coordinateOutputTypeForm = fb.group({
-      format: ['json', Validators.required]
+      format: ['json', Validators.required],
     });
   }
 
   options = {
     layers: [
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      })
+      L.tileLayer(
+        'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+        {
+          maxZoom: 20,
+          attribution:
+            '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+        }
+      ),
     ],
     zoom: 5,
-    center: { lat: -35.307841, lng: 149.12447 }
-  }
+    center: { lat: -35.307841, lng: 149.12447 },
+    zoomControl: false,
+    doubleClickZoom: false,
+  };
 
   coordinateOutputType() {
     return this.coordinateOutputTypeForm.get('format')?.value;
@@ -40,20 +48,37 @@ export class AppComponent implements AfterViewInit {
     return this.coordinatesJson()
       .replaceAll(/((\[)|(\])|(\")|(\{)|(\:)|(lat)|(lng))+/g, '')
       .replaceAll('},', '\n')
-      .replaceAll('}', '')
-      ;
+      .replaceAll('}', '');
   }
-
-
 
   ngAfterViewInit(): void {
     this.map = L.map('map', this.options);
 
-    this.map.on("click", e => {
-      var newLatLng = L.latLng(parseFloat(e.latlng.lat.toFixed(6)), parseFloat(e.latlng.lng.toFixed(6)) % 360);
+    this.map.on('click', (e) => {
+      var newLatLng = L.latLng(
+        parseFloat(e.latlng.lat.toFixed(6)),
+        parseFloat(e.latlng.lng.toFixed(6)) % 360
+      );
       console.log(newLatLng); // get the coordinates
-      var newMarker = L.circleMarker(newLatLng, { radius: 3 }).addTo(this.map);
+      var newMarker = L.circleMarker(newLatLng, {
+        radius: 3,
+        color: '#3F83F8',
+        fillColor: '#3F83F8',
+        fillOpacity: 100,
+      });
+      newMarker.options.className = '!cursor-grab';
+      newMarker.addTo(this.map);
       this.coordinates.push(newLatLng);
+
+      if (this.coordinates.length === 1) {
+        this.line = L.polyline(this.coordinates, { color: '#c084fc' }); //purple-500
+        this.line.options.className = '!cursor-grab';
+        this.line.addTo(this.map);
+      } else {
+        this.map.removeLayer(this.line);
+        this.line.addLatLng(newLatLng);
+        this.line.addTo(this.map);
+      }
     });
   }
 }
